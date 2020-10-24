7a5051ff1e89a2a4e429fc7f00df21fc2bb76379
martitasea <marta.pulido.asin@gmail.com>
2020-10-24 13:24:10 +0200

    Title and frontpage

--- js/index.js
+++ js/index.js
@@ -13,32 +13,40 @@
 // https://source.unsplash.com/
 // https://randomuser.me/api
 
-let container = document.getElementById("conjunto")
+let container = document.getElementById("conjunto");
 fetch("https://randomuser.me/api?results=100")
-    .then(response =>
-        response.json()
-    )
-    .then(personajes => {
-        // HACEMOS UN CONSOLELOG PARA CONOCER LOS DATOS--------------------------------------------------
-        console.log(personajes.results);
-        // METEMOS UN TEMPLATE STRING CON EL CÓDIGO QUE DEBERÍA IR EN HTML-------------------------------
-        personajes.results.map((personas) => {
-            let card = `
+  .then((response) => response.json())
+  .then((personajes) => {
+    // HACEMOS UN CONSOLELOG PARA CONOCER LOS DATOS--------------------------------------------------
+    console.log(personajes.results);
+    // METEMOS UN TEMPLATE STRING CON EL CÓDIGO QUE DEBERÍA IR EN HTML-------------------------------
+    personajes.results.map((personas) => {
+      let card = `
         <figure>
         <img id="face" src="${personas.picture.large}" alt="">
         <div id="datos">
             <h1 id="name">
-                ${personas.name.title} ${personas.name.first} ${personas.name.last}
+                ${personas.name.title} ${personas.name.first} ${
+        personas.name.last
+      }
                 </h1>
             <p id="gender">
-            ${(((personas.gender) == "male") ? '<img class="simGender" src="img/male-01.svg"></img>' : '<img class="simGender" src="img/female-01.svg"></img>')}
+            ${
+              personas.gender == "male"
+                ? '<img class="simGender" src="img/male-01.svg"></img>'
+                : '<img class="simGender" src="img/female-01.svg"></img>'
+            }
             ${personas.gender.toUpperCase()}
             </p>
             <p id="city">
-                ${personas.location.city} ${personas.location.country.toUpperCase()}
+                ${
+                  personas.location.city
+                } ${personas.location.country.toUpperCase()}
             </p>
             <p id="street">
-                ${personas.location.street.number} ${personas.location.street.name}
+                ${personas.location.street.number} ${
+        personas.location.street.name
+      }
             </p>
             <div id="contacto">
                 <p id="email">
@@ -51,23 +59,18 @@ fetch("https://randomuser.me/api?results=100")
         </div >
       </figure >
             `;
-            container.innerHTML += card;
-        })
+      container.innerHTML += card;
+    });
 
-        // AQUÍ DECIMOS QUE EL INNERHTML SEA ADEMÁS DE LO QUE YA ES QUE AÑADA LA CARD--------------------------------
-        // ${((personas.gender)=="male")?<img class="simGender" src=#></img>:<img class="simGender" src=#></img>}
+    // AQUÍ DECIMOS QUE EL INNERHTML SEA ADEMÁS DE LO QUE YA ES QUE AÑADA LA CARD--------------------------------
+    // ${((personas.gender)=="male")?<img class="simGender" src=#></img>:<img class="simGender" src=#></img>}
 
+    // let face = document.createElement("img");
+    // Así se crea un div, el problema es que cuando tienes muchos divs dentro de divs, se complica mucho.
+    // Por ello es mejor que sea un template string
+    // face.src = personajes.results[0].picture.large;
+    // console.log(face.src)
+  });
 
-
-
-        // let face = document.createElement("img");
-        // Así se crea un div, el problema es que cuando tienes muchos divs dentro de divs, se complica mucho. 
-        // Por ello es mejor que sea un template string
-        // face.src = personajes.results[0].picture.large;
-        // console.log(face.src)
-
-    })
-
-
-    // <h1 id="name">${personajes.results[0].name.title} ${personajes.results[0].name.first} ${personajes.results[0].name.last}</h1>
-    // <p id="gener">${personajes.results[0].gender.toUpperCase()} //<img src="${femenino ? "url femenino" : "url masculino"}
+// <h1 id="name">${personajes.results[0].name.title} ${personajes.results[0].name.first} ${personajes.results[0].name.last}</h1>
+// <p id="gener">${personajes.results[0].gender.toUpperCase()} //<img src="${femenino ? "url femenino" : "url masculino"}
